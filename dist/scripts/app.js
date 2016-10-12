f5e6ea3ad2c1c367a30c5da4ba4a8e467e6b19e3
Victoria Fung <victoriahkfung@gmail.com>
2016-10-11 22:46:59 -0700

    assignment3: completed

---
+++ dist/scripts/app.js
@@ -0,0 +1,24 @@
+(function() {
+	function config($stateProvider, $locationProvider){
+		$locationProvider
+			.html5Mode({
+				enable: true,
+				requireBase: false
+		});
+		
+		$stateProvider
+			.state('landing', {
+				url: '/',
+				templateUrl: '/templates/landing.html'
+			})
+			.state('album', {
+				url: '/album',
+				templateUrl: '/template/album.html'
+			});
+	}
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config);
+})();
+
+
