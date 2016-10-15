3d1a30456974acf2e60e49f37a84df85e0b467ca
Victoria Fung <victoriahkfung@gmail.com>
2016-10-15 16:26:15 -0700

    Checkpoint7: completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,12 @@
 (function() {
-	function AlbumCtrl(Fixtures) {
+	function AlbumCtrl(Fixtures, SongPlayer) {
 		this.albumData = Fixtures.getAlbum();
 		this.songs = this.albumData.songs;
+		this.songPlayer = SongPlayer;
+		
 	}
 	
 	angular
 	.module('blocJams')
-	.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
-})();
+	.controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
+})()
