021e1238e23dd7e2fb2e04779d160476bb9da72a
Victoria Fung <victoriahkfung@gmail.com>
2016-10-23 22:55:04 -0700

    checkpoint 11 completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,29 +1,12 @@
 (function() {
-	function timecode() {
-		return function(seconds) {
-			var seconds = Number.parseFloat(seconds);
-			
-			if (Number.isNaN(seconds)) {
-				return '-:--'
-			}
-			
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
+     function timecode() {
+         return function(seconds) {
+             var output = buzz.toTimer(seconds);
+             return output;
+         };
+     }
  
-            var output = minutes + ':';
- 
-            if (remainingSeconds < 10) {
-				output += '0';   
-            }
- 
-            output += remainingSeconds;
-			
-			return output;
-		};
-	}
-	
-	angular
-	.module('blocJams')
-	.filter('timecode', timecode);
-})();
+     angular
+         .module('blocJams')
+         .filter('timecode', timecode);
+ })();
